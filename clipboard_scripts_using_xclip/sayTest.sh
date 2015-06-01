4cc9aef247bedaf5b53ab390094a476bc5fdad08
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-06-01 10:52:00 +0200

    Create sayTest.sh

---
+++ clipboard_scripts_using_xclip/sayTest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# tes file to determine how we can execute a custom bash script in an .sh file from a keyboard shortcut ( hotkey )
+
+# say hello to the world
+spd-say -t male3 -p 100 -r 22 -i -70 "Hello world"
