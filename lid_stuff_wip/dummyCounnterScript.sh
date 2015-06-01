c175b1b567f4008bf3a2bf8f1a6be9af1bc6ba5f
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-06-01 11:26:08 +0200

    Create dummyCounnterScript.sh

---
+++ lid_stuff_wip/dummyCounnterScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# dummy count while in range ( used to see if a program is stopped by that **** lid when it closes )
+
+for i in {1..30}
+do
+  echo "hey there for the $i time"
+  sleep 1
+done
