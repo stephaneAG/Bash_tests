c86e6382d19be13a49c73728859ba2f85c5df6b1
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-06-01 11:58:02 +0200

    Create returnValueOfLastCmdAsSymbolAndUserAndHost.sh

---
+++ promptHacks/returnValueOfLastCmdAsSymbolAndUserAndHost.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#return value visualisation
+PS1="\[\033[01;37m\]\$? \$(if [[ \$? == 0 ]]; then echo \"\[\033[01;32m\]\342\234\223\"; else echo \"\[\033[01;31m\]\342\234\227\"; fi) $(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]\h'; else echo '\[\033[01;32m\]\u@\h'; fi)\[\033[01;34m\] \w \$\[\033[00m\] "
+# Zero is a green smiley and non-zero a red one. So your prompt will smile if the last operation was successful.
