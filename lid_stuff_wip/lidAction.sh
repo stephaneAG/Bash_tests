49d2d780a1d9f24183e25deef7ecafb09263ab01
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-06-01 11:28:08 +0200

    Create lidAction.sh

---
+++ lid_stuff_wip/lidAction.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# lid action handler ( used to see if a program is stopped by that **** lid when it closes )
+
+for i in {1..3}
+do
+  spd-say -t male3 -p 100 -r 22 -i -70 "Lid stuff happening"
+  sleep 1
+done
