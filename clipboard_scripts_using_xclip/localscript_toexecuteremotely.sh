4630b33c6938150aff75b95e93999f539f6e5d09
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-06-01 10:51:10 +0200

    Create localscript_toexecuteremotely.sh

---
+++ clipboard_scripts_using_xclip/localscript_toexecuteremotely.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# this file is intended to be executed on a remote location ( here, a Mac ), without having to it to the remote location first
+
+# say hello using the builtin Mac OS terminal command
+say "Hello bitch"
