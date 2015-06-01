54d4f04cc346bf8a714adb9071db1293e653dfa0
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-06-01 11:29:34 +0200

    Create Screenblankction.sh

---
+++ lid_stuff_wip/Screenblankction.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# lid action handler ( used to see if a program is stopped by that **** lid when it closes )
+
+spd-say -t male3 -p 100 -r 22 -i -70 "They tried to blank your screen, those bastards"
