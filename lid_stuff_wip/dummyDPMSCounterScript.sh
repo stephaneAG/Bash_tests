e163363bfe69d8b36d49e7fd771f6b780344800b
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-06-01 11:26:26 +0200

    Create dummyDPMSCounterScript.sh

---
+++ lid_stuff_wip/dummyDPMSCounterScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# dummy count while in range ( used to see if a program is stopped by that **** lid when it closes )
+
+for i in {1..30}
+do
+  echo "hey there for the $i time"
+  xset dpms force on
+  sleep 1
+done
